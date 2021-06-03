d613977fe2b97a0649e170ba5ca7257a8b03847b
Iryna Stein <isophiestein@gmail.com>
2021-06-03 14:31:14 -0600

    Inititated fetch

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
-document.addEventListener("DOMContentLoaded", function() {});
+document.addEventListener("DOMContentLoaded", function () { });
+
+const listPanel = document.querySelector("#list-panel")
+const list = document.querySelector("#list")
+const showPanel = document.querySelector("#show-panel")
+
+const booksURL = 'http://localhost:3000/books'
+
+function getBooks() {
+    return fetch(booksURL)
+        .then(resp => resp.json())
+        .then(data => renderBooks(data));
+}
+
+function renderBooks(data) {
+    data.map(bookObj => console.log(bookObj))
+}
