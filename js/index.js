e168b292fa4101d0ac39298478e2dea447d3c60c
Iryna Stein <isophiestein@gmail.com>
2021-06-04 13:21:48 -0600

    Finalized book info section +users

--- js/index.js
+++ js/index.js
@@ -44,14 +44,57 @@ function showBookdetails(bookObj) {
     const p = document.createElement('p')
     p.innerText = bookObj['description']
     const usersUl = document.createElement('ul')
-    bookObj.users.map(user => {
-        const userLi = document.createElement('li')
-        userLi.innerText = user.username
-        usersUl.append(userLi)
-        return usersUl;
-    })
-    showPanel.append(title, img, author, subtitle, p, usersUl)
+    let userNames = []
+    bookObj.users.map(user => listUsers(user, usersUl, userNames))
+
+    // bookObj.users.map(user => {
+    //     const userLi = document.createElement('li')
+    //     userLi.innerText = user.username
+    //     usersUl.append(userLi)
+    //     userNames.push({
+    //         "id": user.id,
+    //         "username": user.username
+    //     })
+    //     return usersUl;
+    // })
+    console.log(userNames)
+    let button = document.createElement('button')
+    button.innerHTML = `<id ="like-btn">LIKE`
+    showPanel.append(title, img, author, subtitle, p, usersUl, button)
+    button.addEventListener('click', () => likeBook(userNames))
 }
 
+function listUsers(user, ul, userNames) {
+    const userLi = document.createElement('li');
+    userLi.innerText = user.username;
+    ul.append(userLi)
+    userNames.push({
+        "id": user.id,
+        "username": user.username
+    })
+    return ul
+}
 
+function likeBook() {
+    let button = document.querySelector("#show-panel > button > id")
+    let me = { "id": 1, "username": "pouros" }
+    if (button.innerText === "LIKE") {
+        button.innerHTML = "UNLIKE"
+        //appned li to the ul
+        console.log(`${me.username} liked this book`)
+    }
+    else {
+        button.innerHTML = "LIKE"
+        //remove name from the ul
+        console.log(`${me.username} unliked this book`)
+    }
+
+}
+//fetch POST request function
+// fetch(http://localhost:3000/books/:id ,{
+    //         method: 'PATCH',
+    // body: JSON.stringify({ // add userNames object + me object and send it 
+
+    // })
+    //     }
 
